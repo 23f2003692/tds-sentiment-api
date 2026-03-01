a1892828a0f6047fdf423acec59650d8a45166fb
Anand singh chauhan <23f2003692@ds.study.iitm.ac.in>
2026-03-01 14:13:26 +0530

    added cors middleware

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# 🔥 CORS FIX (VERY IMPORTANT)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CommentRequest(BaseModel):
     comment: str
